f299d9ad1551f200489f6e331bb6c0c1a3fa232e
kvn175 <73406755+kvn175@users.noreply.github.com>
2021-04-09 16:30:41 -0400

    more changes

--- public/labs/lab_5/script.js
+++ public/labs/lab_5/script.js
@@ -1,6 +1,6 @@
 function mapInit() {
-    let mymap = L.map('mapid').setView([51.505, -0.09], 13);
-
+    const mymap = L.map('mapid').setView([51.505, -0.09], 13);
+    
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
@@ -10,7 +10,7 @@ function mapInit() {
     accessToken: 'pk.eyJ1Ijoia3ZuMTc1IiwiYSI6ImNrbmFxam85bDAwc2IydnBoa2owaWtqbHQifQ.jmECV75ko97-7r3QAGVNTw'
     }).addTo(mymap);
     console.log('mymap', mymap);
-    
+
     return mymap;
 }
 
@@ -30,9 +30,9 @@ async function dataHandler(mapObjectFromFunction) {
             console.log('markerLongLat', longLat[0], longLat[1]);
             const marker = L.marker([longLat[1], longLat[0]]).addTo(mapFromMapFunction);
         
-            const appendItem = document.createElement("li");
-            appendItem.classList.add("block";)
-            appendItem.classList.add("list-item");
+            const appendItem = document.createElement('li');
+            appendItem.classList.add('block');
+            appendItem.classList.add('list-item');
             appendItem.innerHTML = `<div class="list-header is-size-5">${item.name}</div><address class="is-size-6">${item.address_line_1}</address>`;
             targetList.append(appendItem);
         });
@@ -41,7 +41,7 @@ async function dataHandler(mapObjectFromFunction) {
 
 async function windowActions() {
     const map = mapInit();
-    await dataHandler(map);
+    // await dataHandler(map);
 }
 
 window.onload = windowActions;
